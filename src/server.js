f326b625399f76e1189cce3ed502417adc0bd83e
DulenL30 <dulenvinjaya21@gmail.com>
2026-03-13 22:41:31 +0530

    Added URL.

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import app from "./app.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // No async loading needed anymore — JSONs are loaded synchronously in the service
 app.listen(PORT, () => {
